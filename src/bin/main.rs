437cfcf4c0184b86ffc3fd7d9afe5d64fbbbffc8
Yuki <yuki.mul.tiplus@gmail.com>
2017-12-04 23:30:08 +0900

    run with thread pool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,21 +4,20 @@ use simple_http_server::request_handler::RequestHandler;
 use simple_http_server::request_parser::RequestParser;
 use simple_http_server::server::Server;
 use simple_http_server::worker::Worker;
+use simple_http_server::ThreadPool;
 
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
-use std::thread;
 
 fn main() {
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8888);
     let listener = TcpListener::bind(addr).unwrap();
 
-    // TODO incoming?
-    match listener.accept() {
-        Ok((socket, _addr)) => {
+    let pool = ThreadPool::new(4);
+    for socket in listener.incoming() {
+        let socket = socket.unwrap();
+        pool.execute(|| {
             let mut server = Server::new(socket, RequestHandler::new(), RequestParser::new());
-            let handle = thread::spawn(move || { server.start(); });
-            handle.join().unwrap();
-        }
-        Err(e) => println!("couldnt get client: {:?}", e),
-    };
+            server.start();
+        });
+    }
 }
